14bddd8cc0a96b39e3104d3fc5e4a28e8acdcd36
Mosesomo <moseswamboga4@gmail.com>
2024-04-03 17:55:04 +0300

    FIFO

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+'''FiFO caching'''
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    '''Caching system inheriting from BaseCaching'''
+
+    def __init__(self):
+        '''Initaializing parent class'''
+
+        super().__init__()
+        self.data_cache = []
+
+    def put(self, key, item):
+        '''Discard the first element if is higher than max items'''
+
+        if key is None or item is None:
+            pass
+        else:
+            len_items = len(self.cache_data)
+            if len_items >= BaseCaching.MAX_ITEMS\
+               and key not in self.cache_data:
+                discard_key = self.data_cache.pop(0)
+                del self.cache_data[discard_key]
+                print('DISCARD: {}'.format(discard_key))
+                del discard_key
+            self.data_cache.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        '''Get an item by key'''
+
+        if key is not None and key in self.cache_data:
+            self.cache_data[key]
+        else:
+            return None
