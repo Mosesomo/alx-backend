03dadbf726b3af2e5e965496357a266f4261b098
Mosesomo <moseswamboga4@gmail.com>
2024-04-12 15:44:39 +0300

    babel

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def display():
+def index():
     '''Flask set up'''
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
